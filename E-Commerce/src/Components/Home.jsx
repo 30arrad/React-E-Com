88d8daba5b53eaf8db2b13fce4698fe90fccac8d
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2025-02-03 20:56:32 +0600

    Add

--- E-Commerce/src/Components/Home.jsx
+++ E-Commerce/src/Components/Home.jsx
@@ -1,24 +1,20 @@
 import HeroSession from "./HeroSession";
-import Items from './Items';
+import Items from "./Items";
 
-const Home = () => {
-    const item = {
-        id: '00001',
-        item_img : `Images/Hoodies/H-1.jpg`,
-        item_Name : `Mens Premium Hoodie Urban - Light Coffee`,
-        item_Old_Price: 2500,
-        item_Price :  1790,
-        item_item_Category : `HOODIES` 
-      }
-    return (
-        <>
-         <img className='HEROIMG' src="banner.png" alt="" style={{width: "100%"}}/> 
-        <HeroSession/>
+const Home = ({ item }) => {
+  return (
+    <>
+      <img
+        className="HEROIMG"
+        src="banner.png"
+        alt=""
+        style={{ width: "100%" }}
+      />
+      <HeroSession />
 
-        <Items key={item.id} item={item}/>
-        </>
-    )
-
-}
+      <Items item={item} />
+    </>
+  );
+};
 
 export default Home;
--- E-Commerce/src/Components/Items.jsx
+++ E-Commerce/src/Components/Items.jsx
@@ -1,33 +1,53 @@
 import { FaRegHeart } from "react-icons/fa";
 import styles from "./Items.module.css";
 
-const Items = ({item}) => {
+const Items = () => {
+  const item = {
+    id: "00001",
+    item_img: `Images/Hoodies/H-1.jpg`,
+    item_Name: `Mens Premium Hoodie Urban - Light Coffee`,
+    item_Old_Price: 2500,
+    item_Price: 1790,
+    item_item_Category: `HOODIES`,
+  };
 
-    
-    return (
-        <center className={styles.itemsDev} >
+  const backendData = () => {
+    fetch("http://localhost:8080/items")
+      .then((res) => res.json())
+      .then(console.log);
+  };
 
+  return (
+    <center className={styles.itemsDev}>
+      <div className="card itemstyle" style={{ width: "18rem" }}>
+        <img
+          src={item.item_img}
+          className="card-img-top imgHover {styles.hover}"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{item.item_Name}</h5>
+          <div className={styles.spanDiv}>
+            <span className={styles.spanX}>৳ {item.item_Old_Price} </span>
+            <span className={styles.spanY}>৳ {item.item_Price}</span>
+          </div>
+          <div className={styles.BUTTON}>
+            <button
+              type="button"
+              className="btn btn-primary buttonBuy BUTTONCARD"
+            >
+              Add to Card
+            </button>
+            <button type="button" className="  buttonBuy BUTTONHEARD">
+              <FaRegHeart />
+            </button>
+          </div>
+        </div>
+      </div>
 
-            <div className="card itemstyle" style={{width: "18rem"}}>
-                <img src={item.item_img} className="card-img-top imgHover {styles.hover}" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{item.item_Name}</h5>
-                    <div className={styles.spanDiv}>
-                    <span className={styles.spanX}>৳ {item.item_Old_Price} </span>
-                    <span className={styles.spanY}>৳ {item.item_Price}</span>
-                    </div>
-                    <div className={styles.BUTTON}  >
-                    <button type="button" className="btn btn-primary buttonBuy BUTTONCARD">Add to Card</button>
-                    <button type="button" className="  buttonBuy BUTTONHEARD"><FaRegHeart /></button>
-                    </div>
-                </div>
-            </div>
-
-
-        </center>
-    )
-}
+      <button onClick={backendData}>BackEnd</button>
+    </center>
+  );
+};
 
 export default Items;
-
-
